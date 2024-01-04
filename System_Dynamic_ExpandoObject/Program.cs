266d740df19699b5f28db02043f652e0b2d4710a
mrunal77 <mtmeshram77@outlook.com>
2024-01-04 09:06:56 +0530

    JsonSerializer used to serialize the dynamic object

--- System_Dynamic_ExpandoObject/Program.cs
+++ System_Dynamic_ExpandoObject/Program.cs
@@ -1,7 +1,12 @@
-﻿dynamic var1 = new System.Dynamic.ExpandoObject();
+﻿
+using System.Text.Json;
+
+dynamic var1 = new System.Dynamic.ExpandoObject();
 var1.name = "test";
 var1.price = 10;
 string json = Newtonsoft.Json.JsonConvert.SerializeObject(var1);
+string json1 = JsonSerializer.Serialize(var1);
 Console.WriteLine(var1); // Prints the System.Dynamic.ExpandoObject
-Console.WriteLine(json); // {"name":"test","price":10}
+Console.WriteLine(json); // Prints json {"name":"test","price":10}
+Console.WriteLine(json1); // Prints json {"name":"test","price":10}
 
