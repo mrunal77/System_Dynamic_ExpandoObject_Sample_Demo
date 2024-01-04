aee6f84859aa82d14233130eddf80aee094c8ba0
mrunal77 <mtmeshram77@outlook.com>
2024-01-04 08:52:16 +0530

    Add project files.

---
+++ System_Dynamic_ExpandoObject/Program.cs
@@ -0,0 +1,7 @@
+﻿dynamic var1 = new System.Dynamic.ExpandoObject();
+var1.name = "test";
+var1.price = 10;
+string json = Newtonsoft.Json.JsonConvert.SerializeObject(var1);
+Console.WriteLine(var1); // Prints the System.Dynamic.ExpandoObject
+Console.WriteLine(json); // {"name":"test","price":10}
+
